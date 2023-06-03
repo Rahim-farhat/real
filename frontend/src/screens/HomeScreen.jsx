11ea4b3ac29b0d089358987a3a9dc6f34568a3c2
Rahim-Farhat <abderrahimfarhat2003@gmail.com>
2023-06-03 14:27:56 +0100

    homescreenupdated

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,41 +1,28 @@
-//import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+//import data from '../data';
 import axios from 'axios';
 
 function HomeScreen() {
-  /*
   const [events, setEvents] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/events');
-      setEvents(result.data);
+      try {
+        const response = await axios.get('/api/events');
+        setEvents(response.data);
+      } catch (error) {
+        console.log(error);
+      }
     };
+
     fetchData();
   }, []);
-  */
-
-  const getJoke = () => {
-    axios.get('https://random-data-api.com/api/v2/users').then((response) => {
-      console.log(response);
-    });
-  };
-  console.log('hey');
-
-  const getapi = () => {
-    axios.get('/api/events').then((response1) => {
-      console.log(response1);
-    });
-  };
-  console.log('yo');
 
   return (
     <div>
-      <button onClick={getJoke}>get the joke</button>
-      <button onClick={getapi}>get the api</button>
       <h1>Featured Events</h1>
       <div className="events">
-        {data.events.map((event) => (
+        {events.map((event) => (
           <div className="event" key={event.slug}>
             <Link to={`/event/${event.slug}`}>
               <p>{event.name}</p>
