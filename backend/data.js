6c0aeabf260d7e1c5a93eb6cd04975ccd2f03e7f
Rahim-Farhat <abderrahimfarhat2003@gmail.com>
2023-06-25 22:39:16 +0100

    b

--- backend/data.js
+++ backend/data.js
@@ -20,6 +20,25 @@ const data = {
         'https://scontent.ftun10-1.fna.fbcdn.net/v/t39.30808-6/352180837_662498392378028_6268851799442282251_n.jpg?stp=dst-jpg_s960x960&_nc_cat=107&ccb=1-7&_nc_sid=340051&_nc_ohc=JfAggZzmkXEAX976xKG&_nc_ht=scontent.ftun10-1.fna&oh=00_AfDYtVtdY62dBTLZHz1JCBwxxo9AGUFemoy-WanURHIg_Q&oe=6490D180',
     },
     {
+      name: 'twoooooo',
+      slug: 'twoo',
+      where: 'Tunis /theThingsLand',
+      categories: ['category1', 'category3'],
+      types: ['competition', 'presentation', 'workshops'],
+      owner: 'someone',
+      target: 'Anyone',
+      price: 'Free Entry',
+      start_d: 26,
+      end_d: 28,
+      start_m: 'June',
+      end_m: 'June',
+      time: '08:30 AM',
+      year: 2023,
+
+      image:
+        'https://scontent.ftun10-1.fna.fbcdn.net/v/t39.30808-6/352180837_662498392378028_6268851799442282251_n.jpg?stp=dst-jpg_s960x960&_nc_cat=107&ccb=1-7&_nc_sid=340051&_nc_ohc=JfAggZzmkXEAX976xKG&_nc_ht=scontent.ftun10-1.fna&oh=00_AfDYtVtdY62dBTLZHz1JCBwxxo9AGUFemoy-WanURHIg_Q&oe=6490D180',
+    },
+    {
       name: '2hello',
       slug: 'hello',
       where: 'Tunis /theThingsLand',
