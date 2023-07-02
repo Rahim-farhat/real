e1424eedd12897fbea8625b23f99bec0d0e6b2b0
Rahim-Farhat <abderrahimfarhat2003@gmail.com>
2023-07-02 15:49:50 +0100

    g2

--- backend/data.js
+++ backend/data.js
@@ -76,6 +76,63 @@ const data = {
       image:
         'https://scontent.ftun10-1.fna.fbcdn.net/v/t39.30808-6/352180837_662498392378028_6268851799442282251_n.jpg?stp=dst-jpg_s960x960&_nc_cat=107&ccb=1-7&_nc_sid=340051&_nc_ohc=JfAggZzmkXEAX976xKG&_nc_ht=scontent.ftun10-1.fna&oh=00_AfDYtVtdY62dBTLZHz1JCBwxxo9AGUFemoy-WanURHIg_Q&oe=6490D180',
     },
+    {
+      name: 'La 7ème édition de Tusdfsdnisia Digsdfital Summit',
+      slug: 'La 7ème édition de Tunisia Digital Summit457989',
+      where: 'Tunis /theThingsLand',
+      categories: ['category1', 'category3'],
+      types: ['competition', 'presentation', 'workshops'],
+      owner: 'someone',
+      target: 'Anyone',
+      price: 'Free Entry',
+      start_d: 26,
+      end_d: 28,
+      start_m: 'June',
+      end_m: 'June',
+      time: '08:30 AM',
+      year: 2023,
+
+      image:
+        'https://scontent.ftun10-1.fna.fbcdn.net/v/t39.30808-6/352180837_662498392378028_6268851799442282251_n.jpg?stp=dst-jpg_s960x960&_nc_cat=107&ccb=1-7&_nc_sid=340051&_nc_ohc=JfAggZzmkXEAX976xKG&_nc_ht=scontent.ftun10-1.fna&oh=00_AfDYtVtdY62dBTLZHz1JCBwxxo9AGUFemoy-WanURHIg_Q&oe=6490D180',
+    },
+    {
+      name: 'La 7ème édition de Tunisia Digital dfdfSummit',
+      slug: 'La 7ème édition de Tunisia Digital Summit658989',
+      where: 'Tunis /theThingsLand',
+      categories: ['category1', 'category3'],
+      types: ['competition', 'presentation', 'workshops'],
+      owner: 'someone',
+      target: 'Anyone',
+      price: 'Free Entry',
+      start_d: 26,
+      end_d: 28,
+      start_m: 'June',
+      end_m: 'June',
+      time: '08:30 AM',
+      year: 2023,
+
+      image:
+        'https://scontent.ftun10-1.fna.fbcdn.net/v/t39.30808-6/352180837_662498392378028_6268851799442282251_n.jpg?stp=dst-jpg_s960x960&_nc_cat=107&ccb=1-7&_nc_sid=340051&_nc_ohc=JfAggZzmkXEAX976xKG&_nc_ht=scontent.ftun10-1.fna&oh=00_AfDYtVtdY62dBTLZHz1JCBwxxo9AGUFemoy-WanURHIg_Q&oe=6490D180',
+    },
+    {
+      name: 'La 7ème éddifsdsdftion de Tunsdfdisia Digital Summit',
+      slug: 'La 7ème édition de Tunisia Digital Summit898565',
+      where: 'Tunis /theThingsLand',
+      categories: ['category1', 'category3'],
+      types: ['competition', 'presentation', 'workshops'],
+      owner: 'someone',
+      target: 'Anyone',
+      price: 'Free Entry',
+      start_d: 26,
+      end_d: 28,
+      start_m: 'June',
+      end_m: 'June',
+      time: '08:30 AM',
+      year: 2023,
+
+      image:
+        'https://scontent.ftun10-1.fna.fbcdn.net/v/t39.30808-6/352180837_662498392378028_6268851799442282251_n.jpg?stp=dst-jpg_s960x960&_nc_cat=107&ccb=1-7&_nc_sid=340051&_nc_ohc=JfAggZzmkXEAX976xKG&_nc_ht=scontent.ftun10-1.fna&oh=00_AfDYtVtdY62dBTLZHz1JCBwxxo9AGUFemoy-WanURHIg_Q&oe=6490D180',
+    },
   ],
 };
 
