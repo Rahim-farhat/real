78d0277a24f3c82004d35889b4206750d6789117
Rahim-Farhat <abderrahimfarhat2003@gmail.com>
2023-08-03 16:24:13 +0100

    normal

--- frontend/src/screens/CategoryScreen.jsx
+++ frontend/src/screens/CategoryScreen.jsx
@@ -30,7 +30,7 @@ function CategoryScreen() {
       try {
         setLoading(true);
         const response = await axios.get(
-          'https://64cbad8605f264000845970c--eventassium.netlify.app/api/events/search',
+          'https://eventassium.netlify.app/api/events/search',
           {
             params: { category },
           }
