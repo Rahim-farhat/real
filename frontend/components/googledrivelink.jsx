e43149486d8267d9c8835c4ed99cc8cc7d1b80e6
Rahim-Farhat <abderrahimfarhat2003@gmail.com>
2023-08-17 14:25:30 +0100

    drivelink

--- frontend/components/googledrivelink.jsx
+++ frontend/components/googledrivelink.jsx
@@ -1,7 +1,7 @@
 export function GoogleDriveLink({ link }) {
   // Check if the link is from Google Drive
   console.log('link=', { link });
-  const isGoogleDriveLink = link && link.includes('drive.google.com');
+  const isGoogleDriveLink = link && link.includes('drive.google.com/file/d/');
 
   // Transform the Google Drive link if applicable
   const transformedLink = isGoogleDriveLink
